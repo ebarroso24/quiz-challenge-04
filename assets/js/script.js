8a1fd8bb93aeb9e95b1099ea3367166900921369
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-07-01 12:20:07 -0400

    made edits to quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
-const questions = [
+var questions = [
     {
-        question: "What is the largest animal in the world?",
+        question: "What describes an object that represents a true value?",
         answers: [
-            {text: "Shark", correct: false},
-            {text: "Blue Wale", correct: true},
-            {text: "Elephant", correct: false},
-            {text: "Dog", correct: false},
+            {text: "Array", correct: false},
+            {text: "Boolean", correct: true},
+            {text: "Variable", correct: false},
+            {text: "Loops", correct: false},
         ]
     },
     {
-        question: "What is the best animal in the world?",
+        question: "What is a useful debugging tool that will print text to the console?",
         answers: [
-            {text: "Dog", correct: true},
-            {text: "Cat", correct: false},
-            {text: "Turtle", correct: false},
-            {text: "Hamster", correct: false},
+            {text: "console.log", correct: true},
+            {text: "a computer keyboard", correct: false},
+            {text: "a function", correct: false},
+            {text: "an event listener", correct: false},
         ]
     },
     {
-        question: "What is the most populated city in the U.S.?",
+        question: "When opening a webpage, the browser creates a tree of objects that represents an open page. This is called ...?",
         answers: [
-            {text: "Los Angeles", correct: false},
-            {text: "Chicago", correct: false},
-            {text: "Phoenix", correct: false},
-            {text: "New York City", correct: true},
+            {text: " a Website", correct: false},
+            {text: "JavaScript", correct: false},
+            {text: "HTML", correct: false},
+            {text: "The Document Object Model (DOM)", correct: true},
         ]
     },
     {
@@ -36,19 +36,19 @@ const questions = [
         ]
     },
     {
-        question: "What is the only professional baseball team in Arizona?",
+        question: "You would add this to attach an event handler to a specified element (hint: Makes a button work)?",
         answers: [
-            {text: "Angels", correct: false},
-            {text: "White Sox", correct: false},
-            {text: "Diamondbacks", correct: true},
-            {text: "Athletics", correct: false},
+            {text: ".setAttribute", correct: false},
+            {text: ".addEventHandler", correct: false},
+            {text: ".addEventListener", correct: true},
+            {text: ".addFunctionListener", correct: false},
         ]
     },
 ]
 
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
+var questionElement = document.getElementById("question");
+var answerButtons = document.getElementById("answer-buttons");
+var nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -67,7 +67,7 @@ function showQuestion(){
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
+        var button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
@@ -85,8 +85,8 @@ function showQuestion(){
 }
 
 function selectAnswer(e){
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
+    var selectedBtn = e.target;
+    var isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
         score++;
