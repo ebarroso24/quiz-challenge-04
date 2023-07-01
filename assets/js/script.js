2293055ca8412b0ba8c3f82a8866d72aff29db28
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-07-01 14:54:18 -0400

    added a timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+var win = document.querySelector(".win");
+var lose = document.querySelector(".lose");
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+
+var winCounter = 0;
+var loseCounter = 0;
+var isWin = false;
+var timer;
+var timerCount;
+
+
+//Quiz Questions and answers.
 var questions = [
     {
         question: "What describes an object that represents a true value?",
@@ -12,9 +25,9 @@ var questions = [
         question: "What is a useful debugging tool that will print text to the console?",
         answers: [
             {text: "console.log", correct: true},
-            {text: "a computer keyboard", correct: false},
+            {text: "keyboard", correct: false},
             {text: "a function", correct: false},
-            {text: "an event listener", correct: false},
+            {text: "search engine", correct: false},
         ]
     },
     {
@@ -23,7 +36,7 @@ var questions = [
             {text: " a Website", correct: false},
             {text: "JavaScript", correct: false},
             {text: "HTML", correct: false},
-            {text: "The Document Object Model (DOM)", correct: true},
+            {text: "The DOM", correct: true},
         ]
     },
     {
@@ -31,28 +44,28 @@ var questions = [
         answers: [
             {text: "DevTools", correct: false},
             {text: "Inspect Tool", correct: true},
-            {text: "Google the answer", correct: false},
-            {text: "Ask a collegue to check your work", correct: false},
+            {text: "Google answer", correct: false},
+            {text: "Ask a collegue", correct: false},
         ]
     },
     {
         question: "You would add this to attach an event handler to a specified element (hint: Makes a button work)?",
         answers: [
-            {text: ".setAttribute", correct: false},
-            {text: ".addEventHandler", correct: false},
-            {text: ".addEventListener", correct: true},
-            {text: ".addFunctionListener", correct: false},
+            {text: "Attribute", correct: false},
+            {text: "Inspect", correct: false},
+            {text: "Event Listener", correct: true},
+            {text: "Function", correct: false},
         ]
     },
 ]
-
+//Naming he IDs and calling them.
 var questionElement = document.getElementById("question");
 var answerButtons = document.getElementById("answer-buttons");
 var nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
-
+//Next button appears after an answer selection.
 function startQuiz(){
     currentQuestionIndex = 0;
     score = 0;
@@ -83,7 +96,6 @@ function showQuestion(){
             answerButtons.removeChild(answerButtons.firstChild)
         }
 }
-
 function selectAnswer(e){
     var selectedBtn = e.target;
     var isCorrect = selectedBtn.dataset.correct === "true";
@@ -104,10 +116,99 @@ function selectAnswer(e){
 
 function showScore(){
     resetState();
-    questionElement.innerHTML = 'You scored ${score} out of ${questions.length}!';
+    questionElement.innerHTML = 'Score:  !';
     nextButton.innerHTML = "Play Again"
     nextButton.style.display = "block"; 
 }
+// The init function is called when the page loads 
+function init() {
+    getWins();
+    getlosses();
+  }
+   // The startGame function is called when the start button is clicked
+   function startGame() {
+    isWin = false;
+    timerCount = 20;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    
+    startTimer()
+  }
+  
+  // The winGame function is called when the win condition is met
+  function winGame() {
+    
+    winCounter++
+    startButton.disabled = false;
+    setWins()
+  }
+  
+  // The loseGame function is called when timer reaches 0
+  function loseGame() {
+    
+    loseCounter++
+    startButton.disabled = false;
+    setLosses()
+  }
+  
+  // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+  function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          winGame();
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        loseGame();
+      }
+    }, 1000);
+  }
+  // Updates win count on screen and sets win count to storage
+function setWins() {
+    win.textContent = winCounter;
+    localStorage.setItem("winCount", winCounter);
+  }
+  
+  // Updates lose count on screen and sets lose count to storage
+  function setLosses() {
+    lose.textContent = loseCounter;
+    localStorage.setItem("loseCount", loseCounter);
+  }
+  
+  // These functions are used by init
+  function getWins() {
+    // Get stored value from client storage, if it exists
+    var storedWins = localStorage.getItem("winCount");
+    // If stored value doesn't exist, set counter to 0
+    if (storedWins === null) {
+      winCounter = 0;
+    } else {
+      // If a value is retrieved from client storage set the winCounter to that value
+      winCounter = storedWins;
+    }
+    //Render win count to page
+    win.textContent = winCounter;
+  }
+  
+  function getlosses() {
+    var storedLosses = localStorage.getItem("loseCount");
+    if (storedLosses === null) {
+      loseCounter = 0;
+    } else {
+      loseCounter = storedLosses;
+    }
+    lose.textContent = loseCounter;
+  }
 
 
 
@@ -130,3 +231,22 @@ nextButton.addEventListener("click", ()=>{
 });
 
 startQuiz();
+ // Attach event listener to start button to call startGame function on click
+ startButton.addEventListener("click", startGame);
+
+ // Calls init() so that it fires when page opened
+ init();
+ 
+ // Bonus: Add reset button
+ var resetButton = document.querySelector(".reset-button");
+ 
+ function resetGame() {
+   // Resets win and loss counts
+   winCounter = 0;
+   loseCounter = 0;
+   // Renders win and loss counts and sets them into client storage
+   setWins()
+   setLosses()
+ }
+ // Attaches event listener to button
+ resetButton.addEventListener("click", resetGame);
